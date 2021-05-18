00e64f6c8055a4cf7b1cd0499e296e2fc7ae4f4d
iam3mer <jhstbarrera@utp.edu.co>
2021-05-17 20:04:18 -0500

    uso de for

---
+++ Unidad 3/ciclosFor.py
@@ -0,0 +1,22 @@
+# for
+# Para X, haga Y
+
+#print(range(10))
+
+#for num in range(5):
+#    print(f'Esta es la iteracion numeró: {num+1}')
+
+#numeros = [1,2,3,4,5]
+#for num in numeros:
+#    print(f'Esta es la iteracion numeró: {num}')
+
+#nombres = ['Juan', 'Maria', 'Carlos', 'Patria']
+#for nombre in nombres:
+#    print(nombre)
+
+#frase = "El mejor momento para plantar un árbol era hace 20 años. El segundo mejor momento es AHORA."
+#palabras = frase.split()
+#print(palabras)
+#Contar la longitud de cada una de las palabras
+#for palabra in palabras:
+#    print(f'La palabra "{palabra}" tiene una longitud de {len(palabra)}')
