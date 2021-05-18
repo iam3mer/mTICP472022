a0fc0bc7361f47168d17701a13ea8b24b08b3db4
iam3mer <jhstbarrera@utp.edu.co>
2021-05-18 18:17:05 -0500

    factorial usando while y for

--- Unidad 3/factorial.py
+++ Unidad 3/factorial.py
@@ -6,4 +6,13 @@ def factorial(num:int=0): # n! = (n-(n-1)) * (n-(n-2))) * (n-(n-3)) * ... * (n)
         num_act = num_act + 1
     return salida
 
+#print(factorial(1))
+
+def factorial(n: int):
+    num = 1
+    for i in range(1, n + 1): # [1,2,3,4,5]
+        num *= i # num = num * i
+
+    return num
+
 print(factorial(1))
