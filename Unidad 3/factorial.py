b5724d982fdbd14af9912e7a5ef9c36eb7d9de7f
iam3mer <jhstbarrera@utp.edu.co>
2021-05-18 20:00:55 -0500

    factorial

--- Unidad 3/factorial.py
+++ Unidad 3/factorial.py
@@ -8,11 +8,11 @@ def factorial(num:int=0): # n! = (n-(n-1)) * (n-(n-2))) * (n-(n-3)) * ... * (n)
 
 #print(factorial(1))
 
+# Factorial propuesto por la clase P47
 def factorial(n: int):
     num = 1
     for i in range(1, n + 1): # [1,2,3,4,5]
         num *= i # num = num * i
-
     return num
 
 print(factorial(1))
