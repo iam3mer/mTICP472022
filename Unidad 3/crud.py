3193823898df936d817474871f365d0227c871ff
iam3mer <jhstbarrera@utp.edu.co>
2021-05-21 12:03:32 -0500

    crud: agregarProducto y mostrarInfoProducto

--- Unidad 3/crud.py
+++ Unidad 3/crud.py
@@ -14,7 +14,7 @@ estructuraProductos = {
 }
 
 dbProductos = {
-    'Gasesosa Pepsi': {
+    'Gaseosa Pepsi': {
         'categoria': 'Alimentos',
         'marca': 'Pepsi',
         'precio': 5000,
@@ -36,6 +36,40 @@ dbProductos = {
 }
 
 
+def mayorQueCero(msjI:str,msjO:str)->int:
+    while True:
+        valor = input(msjI)
+        if valor.isdigit():
+            valor = int(valor)
+            if valor > 0:
+                return valor
+            else:
+                print(msjO)
+
+
+def verificarElemento(lista:list, msj:str)->str:
+    while True:
+        print(lista)
+        valor = input(msj)
+        if valor in lista:
+            return valor
+
+
+def comprobarFecha(msj:str)->str:
+    while True:
+        fecha = input(msj) # dd/mm/yy
+        if len(fecha) == 8:
+            if fecha[2] == '/' and fecha[5] == '/':
+                auxFecha = fecha.split('/') # ['dd', 'mm', 'yy']
+                if auxFecha[0] == 'dd':
+                    continue
+                elif auxFecha[0].isdigit and auxFecha[1].isdigit and auxFecha[2].isdigit:
+                    if int(auxFecha[0]) > 0 and int(auxFecha[0]) < 31:
+                        if int(auxFecha[1]) > 0 and int(auxFecha[1]) < 13:
+                            if int(auxFecha[2]) > 20:
+                                return fecha
+
+
 # CREATE
 def agregarProducto(db:dict)->dict:
     categorias = ['Nuevos', 'Estrella', 'Temporada', 'Descuento', 'Alimentos', 'Otros']
@@ -45,36 +79,31 @@ def agregarProducto(db:dict)->dict:
 
     llaveProducto = input('Ingrese el nombre del producto: ')
 
-    categoria = input('Ingrese la categoria: ')
+    msj = 'Ingrese la categoria: '
+    categoria = verificarElemento(categorias, msj)
 
-    marca = input('Ingrese la marca: ')
+    msj = 'Ingrese la marca: '
+    marca = verificarElemento(marcas, msj)
 
-    while True:
-        precio = input('Ingrese el precio: ')
-
-        if precio > 0:
-            break
-        else:
-            print('Debe ingresar un precio mayor a 0')
+    msjI = 'Ingrese el precio: '
+    msjO = 'Debe ingresar un precio mayor a 0'
+    precio = mayorQueCero(msjI,msjO)
 
     while True:
         comestible = input('El producto es comnestibe? [S/s] Sí, [N/n] No: ')
 
         if comestible == 'S' or comestible == 's':
-            fechaVencimiento = input('Ingrese la fecha de vencimiento: ')
+            msj = 'Ingrese la fecha de vencimiento: '
+            fechaVencimiento = comprobarFecha(msj)
             comestible = {'perecedero': True, 'fechaVencimiento': fechaVencimiento}
             break
         elif comestible == 'N' or comestible == 'n':
             comestible = {'perecedero': False}
             break
 
-    while True:
-        inventario = input('Ingrese la cantidad de elementos a añadir: ')
-
-        if inventario > 0:
-            break
-        else:
-            print('El inventario debe ser por lo menos 1')
+    msjI = 'Ingrese la cantidad de elementos a añadir: '
+    msjO = 'El inventario debe ser por lo menos 1'
+    inventario = mayorQueCero(msjI,msjO)
 
     producto = {
             'categoria': categoria,
@@ -88,4 +117,26 @@ def agregarProducto(db:dict)->dict:
 
     return db
 
-print(agregarProducto(dbProductos))
+# READ
+def mostrarInfoProducto(db:dict)->None:
+    print('Consulta de producto')
+    productos = list(db.keys()) # Consulto las llaves y las almaceno en una lista 
+    msj = 'Ingrese el nombre del producto a mostrar: '
+    producto = verificarElemento(productos, msj)
+
+    #print(db[producto])
+
+    print(f'El producto consultado es: {producto}')
+    print(f'Pertenece a la categoria: {db[producto]["categoria"]}')
+    print(f'La marca \'del\' producto es: {db[producto]["marca"]}')
+    auxPerecedero = db[producto]['comestible']['perecedero']
+    print(f'Es comestible? {auxPerecedero}')
+    if auxPerecedero == True:
+        print(f'Fecha de vencimiento {db[producto]["comestible"]["fechaVencimiento"]}')
+    print(f'Unidades en inventario: {db[producto]["inventario"]}')
+
+#baseProductos = agregarProducto(dbProductos)
+#baseProductos = agregarProducto(baseProductos)
+#baseProductos = agregarProducto(baseProductos)
+
+mostrarInfoProducto(dbProductos)
