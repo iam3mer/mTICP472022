e614a686b15766c6ba6db54681e6c47ac328a83d
iam3mer <jhstbarrera@utp.edu.co>
2021-05-25 20:59:46 -0500

    comprension

--- Unidad 4/compresion.py
+++ Unidad 4/compresion.py
@@ -48,5 +48,80 @@ def esPar(num:int)->int:
         return num
 
 numerosPares = [num for num in range(0,101) if esPar(num) != None]
-print(numerosPares)
+#print(numerosPares)
 
+# Area de un cuadrado
+ladoCuadrados = [2,4,6,8,10,12,14,16]
+areaCuadrados = [l*l for l in ladoCuadrados]
+#print(areaCuadrados)
+
+# Construir una funcion que retorne una lista con las llaves de un diccionario (no usar keys)
+diccionario = {1:'a', 2:'b', 3:'c'}
+#print(list(diccionario.keys()))
+
+keys = []
+def myKeys(diccionario:dict)->list:
+    for key in diccionario:
+        keys.append(key)
+    return keys
+#print(myKeys(diccionario))
+
+keys = [key for key in diccionario]
+#print(keys)
+
+# Construir una funcion que retorne una lista con los valores de un diccionario (no usar values)
+diccionario = {1:'a', 2:'b', 3:'c'}
+#print(list(diccionario.values()))
+
+values = []
+def myValues(diccionario:dict)->list:
+    for key in diccionario:
+        values.append(diccionario[key])
+    return values
+#print(myValues(diccionario))
+
+values = [diccionario[key] for key in diccionario]
+#print(values)
+
+# Encontrar elementos repetidos en una lista
+a = ['Andres', 'Julian', 'Edison', 'Edwin', 'Felipe']
+b = ['Francisco', 'Juan', 'Andres', 'Julian', 'Felipe']
+
+repetidos = []
+def repetido(a:list,b:list)->list:
+    for x in a:
+        if x in b:
+            repetidos.append(x)
+    return repetidos
+#print(repetido(a,b))
+
+repetidos = [x for x in a if x in b]
+#print(repetidos)
+
+repetidos = []
+def repetido(a:list,b:list)->list:
+    for x in a:
+        for y in b:
+            if x == y:
+                repetidos.append(x)
+    return repetidos
+#print(repetido(a,b))
+
+repetidos = [x for x in a for y in b if x == y]
+#print(repetidos)
+
+# Tablas de multiplicar
+# tablasDict = {2: [4,6,8,10,12,14,16,18], 3: []}
+tablasDict = {}
+def tablas(num:int)->dict:
+    for n in range(2,num+1):
+        auxLista = []
+        for z in range(2,10):
+            auxLista.append(n*z)
+        tablasDict[f'Tabla del {n}'] = auxLista
+    return tablasDict
+
+#print(tablas(10))
+
+tablasDict = {f'Tabla del {n}': [n*z for z in range(2,10)] for n in range(2,11)}
+print(tablasDict)
