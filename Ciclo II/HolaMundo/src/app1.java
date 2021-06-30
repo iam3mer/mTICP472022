714894158130a1200aac70ebf98f124c0a5fbd94
iam3mer <jhstbarrera@utp.edu.co>
2021-06-29 19:02:01 -0500

    Unidad 1

---
+++ Ciclo II/HolaMundo/src/app1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class app1 {
+    public static void main(String[] args) throws Exception {
+    
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa tu apellido: ");
+        String apellido = sc.nextLine();
+
+        System.out.println("Hello, "+args[0]+" "+apellido+"!");
+    }
+}
