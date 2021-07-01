9b805cb97a1e990c33fc2c43a2685b225190c6fe
iam3mer <jhstbarrera@utp.edu.co>
2021-07-01 17:56:13 -0500

    ejercicios unidad 1

---
+++ Ciclo II/Unidad 1/ejercicios/src/main/java/xyz/iam3mer/ejerciciosp47/baorjs/Ejercicio1.java
@@ -0,0 +1,54 @@
+package xyz.iam3mer.ejerciciosp47.baorjs;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class Ejercicio1 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println( "Ingrese una fecha: " ); // 12/06/1990
+        String fecha = input.nextLine();
+        input.close();
+
+        int indiceSeparador = fecha.indexOf('/');
+
+        int dia = Integer.parseInt(fecha.substring(0,indiceSeparador));
+
+        int indiceSeparador2 = fecha.lastIndexOf('/');
+
+        int mes = Integer.parseInt(fecha.substring(indiceSeparador+1,indiceSeparador2));
+
+        int anio = Integer.parseInt(fecha.substring(indiceSeparador2+1, fecha.length()));
+
+        int numeroSuerte = dia + mes + anio;
+
+        int len = String.valueOf(numeroSuerte).length();
+
+        int aux = 0;
+        int digito = 0;
+
+        /*
+        for (int i = 0; i < len ;i++)
+        {
+            digito = numeroSuerte % 10;
+            aux += digito;
+            numeroSuerte /= 10;
+        }
+        */
+
+        while(numeroSuerte!=0){
+            digito = numeroSuerte % 10;
+            aux += digito;
+            numeroSuerte /= 10;
+        }
+    
+        System.out.printf("Tu numero de la suerte es: %d", aux);
+        
+    }
+}
