da9cdd03d0de035d1e1f29c0811e622f33e73559
iam3mer <jhstbarrera@utp.edu.co>
2021-07-06 18:40:41 -0500

    opcion 1 notas

--- Ciclo II/Unidad 2/notas/src/main/java/utp/misiontic2022/c2/p47/notas/App.java
+++ Ciclo II/Unidad 2/notas/src/main/java/utp/misiontic2022/c2/p47/notas/App.java
@@ -11,14 +11,27 @@ public class App
     public static void main( String[] args )
     {
 
-        Double auxNota = Double.parseDouble(args[0]);
+        Double auxNota = Double.parseDouble(args[1]);
+        Double promedio = 0.0;
+        Integer indNota = 1;
 
-        for (int i = 1; i < args.length; i++) {
+        for (int i = 2; i < args.length; i++) {
             if (Double.parseDouble(args[i]) < auxNota) {
                 auxNota = Double.parseDouble(args[i]);
+                indNota = i;
             }
         }
 
-        System.out.println(auxNota);
+        args[indNota] = "0";
+
+        for (int i = 1; i < args.length; i++) {
+            promedio += Double.parseDouble(args[i]);
+        }
+
+        promedio /= args.length -2;
+        promedio *= 0.05;
+
+        System.out.printf("El promedio del estudiante con id %s es: %.2f",
+                        args[0], promedio);
     }
 }
