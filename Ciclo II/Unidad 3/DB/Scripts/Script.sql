a44ba70d7acb7f09dfe3e4cfd0f6359cc7b1e0f8
iam3mer <jhstbarrera@utp.edu.co>
2021-08-06 13:51:17 -0500

    SQL

--- Ciclo II/Unidad 3/DB/Scripts/Script.sql
+++ Ciclo II/Unidad 3/DB/Scripts/Script.sql
@@ -1,1 +1,3 @@
 SELECT count() FROM sales s JOIN book b WHERE s.id_book = b.id AND b.isbn = 'fdsg8df89g7df987g'; 
+
+SELECT isbn, title, year, amount FROM book b JOIN stock s ON b.id = s.id_book WHERE s.amount > 0;
