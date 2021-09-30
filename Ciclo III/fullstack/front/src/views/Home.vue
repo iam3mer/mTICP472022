037b7bcb2fec29c913a7eee2d87f8e212b3d46c4
iam3mer <jhstbarrera@utp.edu.co>
2021-09-30 16:44:12 -0500

    frontend

---
+++ Ciclo III/fullstack/front/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <Table />
+</template>
+
+<script>
+  import Table from '@/components/Table'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      Table,
+    },
+  }
+</script>
---
+++ Ciclo III/fullstack/front/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+
+  devServer: {
+    proxy: "http://localhost:2021"
+  }
+}
